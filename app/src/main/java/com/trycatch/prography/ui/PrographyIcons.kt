d96de53f752ba5298c7a3c6318d58df22f7d3f2b
trycatch <trycatch98@gmail.com>
2025-02-19 08:07:13 +0900

    feat: 디자인 시스템 아이콘 추가

---
+++ app/src/main/java/com/trycatch/prography/ui/PrographyIcons.kt
@@ -0,0 +1,12 @@
+package com.trycatch.prography.ui
+
+import com.trycatch.prography.R
+
+object PrographyIcons {
+    val X = R.drawable.ic_x
+    val Bookmark = R.drawable.ic_bookmark
+    val Download = R.drawable.ic_download
+    val Cards = R.drawable.ic_cards
+    val House = R.drawable.ic_house
+    val Information = R.drawable.ic_information
+}
